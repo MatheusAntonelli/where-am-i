f97ba53f6167196cd8c28cffa44a91dca0b048ce
MatheusAntonelli <matheusantonelli12@gmail.com>
2022-07-06 06:16:07 -0300

    First Commit on Where-am-i

---
+++ src/component/header/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='navbar-section'>
+       <img src="./img/Where-am-I.svg" alt="" srcset="" className='logo-img'/>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/component/leaflet/LeafletMap.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import {MapContainer, Marker, Popup, TileLayer, useMap, useMapEvent} from 'react-leaflet';
+import './LeafletMap.css'
+
+
+function LocationMarker() {
+    const [position, setPosition] = useState(null)
+  const map = useMapEvent({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+
+
+function LeafletMap() {
+
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+            <LocationMarker/>
+        </MapContainer>
+    )
+}
+
+export default LeafletMap
